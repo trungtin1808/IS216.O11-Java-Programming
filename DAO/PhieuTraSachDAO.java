4f6a04bc8cacb5649c4205c77cb4d3d394d0a802
trungtin1808 <154118963+trungtin1808@users.noreply.github.com>
2023-12-20 10:08:50 +0700

    Update PhieuTraSachDAO.java

--- DAO/PhieuTraSachDAO.java
+++ DAO/PhieuTraSachDAO.java
@@ -8,8 +8,10 @@ package DAO;
  *
  * @author Admin
  */
-import Object.PhieuTraSach;
-
+import Objects.PhieuTraSach;
+import Objects.ChiTietTraSach;
+import java.util.*;
+import java.sql.Date;
 /**
  *
  * @author Admin
@@ -20,31 +22,122 @@ import java.util.ArrayList;
 import java.util.logging.Level;
 import java.util.logging.Logger;
 
-public class PhieuTraSachDAO {
+public class PhieuTraSachDao {
     public static Connection conn = Connect.getConnect();
-
-   
-    public static boolean KTPhieuTra(int mAPHIEUMUON){
-        String sql="select MAPHIEUMUONSACH from PHIEUMUONSACH WHERE MAPHIEUMUONSACH=?";
-        try (PreparedStatement stm = conn.prepareStatement(sql);) {
-                stm.setInt(1,mAPHIEUMUON);
-                ResultSet rs = stm.executeQuery();
-                return rs.next();
+    public static List<Integer> getlistmAPHIEUMUON(){
+        List<Integer> list=new ArrayList<>();
+        String sql="select MAPHIEUMUONSACH from PHIEUMUONSACH";
+         try (PreparedStatement stm = conn.prepareStatement(sql)) {    
+             ResultSet rs = stm.executeQuery();
+             while(rs.next()){
+                 int MAPHIEUMUONSACH=rs.getInt("MAPHIEUMUONSACH");
+                 list.add(MAPHIEUMUONSACH);
+             }   
+                
+            } catch (SQLException e) {
+                throw new ArithmeticException(e.getMessage());
+            }
+         
+         
+         return list;
+        
+        
+    }
+    public static int getmADOCGIA(int MAPHIEUMUONSACH){
+        int i=0;
+        String sql="select MADOCGIA from PHIEUMUONSACH where MAPHIEUMUONSACH=?";
+         try (PreparedStatement stm = conn.prepareStatement(sql)) { 
+             stm.setInt(1, MAPHIEUMUONSACH);
+             ResultSet rs = stm.executeQuery();
+             if(rs.next()){
+                 i=rs.getInt("MADOCGIA");
+             }
+             
+               
+                
             } catch (SQLException e) {
                 throw new ArithmeticException(e.getMessage());
             }
         
         
+        return i;
     }
+    public static String gettENDOCGIA(int mADOCGIA){
+        String hOTEN=null;
+        String sql="select HOTEN from DOCGIA where MADOCGIA=?";
+         try (PreparedStatement stm = conn.prepareStatement(sql)) { 
+             stm.setInt(1, mADOCGIA);
+             ResultSet rs = stm.executeQuery();
+             if(rs.next()){
+                 hOTEN=rs.getString("HOTEN");
+             }
+             
+                  
+            } catch (SQLException e) {
+                throw new ArithmeticException(e.getMessage());
+            }
+        
+        
+        return hOTEN;
+    }
+    public static List<PhieuTraSach> getlistPTS(){
+        List<PhieuTraSach> list=new ArrayList<>();
+        String sql="select* from PHIEUTRASACH";
+        try (PreparedStatement stm = conn.prepareStatement(sql)) { 
+             ResultSet rs = stm.executeQuery();
+             while(rs.next()){
+                 int mAPHIEUTRA=rs.getInt("MAPHIEUTRA");
+                 int mAPHIEUMUONSACH=rs.getInt("MAPHIEUMUONSACH");
+                 Date nGAYTRA=rs.getDate("NGAYTRA");
+                 int sONGAYTRATRE=rs.getInt("SONGAYTRATRE");
+                 int tIENPHATKINAY=rs.getInt("TIENPHATKINAY");
+                 PhieuTraSach A=new PhieuTraSach(mAPHIEUMUONSACH,nGAYTRA,mAPHIEUTRA,sONGAYTRATRE,tIENPHATKINAY);
+                 list.add(A);
+             }
+             
+                  
+            } catch (SQLException e) {
+                throw new ArithmeticException(e.getMessage());
+            }
+        
+        return list;
+        
+    }
+    public static List<ChiTietTraSach> getCTTS(int mAPHIEUTRA){
+        List<ChiTietTraSach> list=new ArrayList<>();
+        String sql="select* from CTTS where MAPHIEUTRA=?";
+        try (PreparedStatement stm = conn.prepareStatement(sql)) { 
+            
+             stm.setInt(1, mAPHIEUTRA);
+             ResultSet rs = stm.executeQuery();
+             while(rs.next()){
+                 int mADAUSACH=rs.getInt("MADAUSACH");
+                 int tIENPHAT=rs.getInt("TIENPHAT");
+                 ChiTietTraSach A=new ChiTietTraSach(mADAUSACH,mAPHIEUTRA,tIENPHAT);
+                 
+                 list.add(A);
+             }
+             
+                  
+            } catch (SQLException e) {
+                throw new ArithmeticException(e.getMessage());
+            }
+        
+        
+        return list;
+    }
+   
+    
     public static void InsertPhieuTraSach(PhieuTraSach pts){
         String sql="{call THEMPTS(?, ?)}";
         try (CallableStatement stmt = conn.prepareCall(sql);) {
-                stmt.setInt(1,pts.getMaPhieuMuonSach());
-                stmt.setDate(2, pts.getNgayTra());
+                stmt.setInt(1,pts.getmAPHIEUMUON());
+                stmt.setDate(2, pts.getnGAYTRA());
                 stmt.execute();
             } catch (SQLException e) {
                 throw new ArithmeticException(e.getMessage());
             }
     }
 }
+
 
